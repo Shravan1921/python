fa4d5c538e4c20650fb5cfe2201237bf106a2ea9
Shravan1921 <sawsundargirish1999@gmail.com>
2025-07-25 08:50:11 +0530

    .

---
+++ 03_ctrl_stmts/conversion.py
@@ -0,0 +1,18 @@
+
+# Type conversion
+a=10  #int 
+b=3.5 #float
+c=a+b # int/float
+
+print(type(c))
+
+d=3.14 #float
+e=int(d)
+print(type(e))
+print(e)
+
+# Type casting
+x=100
+y=float(x)
+print(type(y))
+print(y)
